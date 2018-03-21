e6c2e69b503d60835cc4526f916d88c1113bf9b3
jineryao <414070723@qq.com>
2018-03-21 14:13:56 +0800

    [修改] 完善admin基础模板

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -1,15 +1,18 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue"
+import Router from "vue-router"
+
+import Login from "@/view/Login.vue"
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+    mode: "history",
+    scrollBehavior: (to, from, savedPosition) => savedPosition || { x: 0, y: 0 },
+    routes: [
+        {
+            path: "/",
+            name: "Login",
+            component: Login
+        }
+    ]
 })
